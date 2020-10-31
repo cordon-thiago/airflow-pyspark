895b69f798e8102625ce364daa74b214823abb3b
cordon-thiago <thiaguscr@gmail.com>
2020-10-31 09:43:39 +0000

    Added volumes to jupyter container to map spark scripts and data directory

--- spark-scripts/hello-world.py
+++ spark-scripts/hello-world.py
@@ -5,4 +5,8 @@ logFile = "airflow/airflow.cfg"
 logData = sc.textFile(logFile).cache()
 numAs = logData.filter(lambda s: 'a' in s).count()
 numBs = logData.filter(lambda s: 'b' in s).count()
+print("#########################################################")
+print("OUTPUT")
+print("#########################################################")
 print("Lines with a: {}, lines with b: {}".format(numAs, numBs))
+print("#########################################################")
